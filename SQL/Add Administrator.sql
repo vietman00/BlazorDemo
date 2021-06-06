a34a0429541bcee845791fba47ab717ec019ef28
Thomas Vo <vietman00@hotmail.com>
2021-06-06 18:46:40 -0400

    Initial..

---
+++ SQL/Add Administrator.sql
@@ -0,0 +1,22 @@
+﻿-- Add Administrator Role if not already present..
+if (not exists(select top 1 * from aspnetroles where name = 'Administrator'))
+begin
+	insert into aspnetroles (id, name, normalizedname, concurrencystamp)
+	values(1, 'Administrator', 'Administrator', newid());
+end
+
+-- Get userId and assign Administrator Role..
+declare @userId uniqueidentifier
+declare @adminId int
+
+select @userId = Id from aspnetusers where email = '' -- Email addressed used when registering on the site..
+select @adminId = Id from aspnetroles where normalizedname = 'Administrator'
+
+-- Show Ids to make sure we have something..
+select @userId, @adminId
+
+if (@userId IS NOT NULL and @adminId IS NOT NULL)
+begin
+	insert into aspnetuserroles(UserId, RoleId)
+	values(@userId, @adminId)
+end
---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BlazorDemo.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
